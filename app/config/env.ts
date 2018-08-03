2ee7e05d6463e927a8c2ff1e86885fb51f8e73c7
coyotte508 <coyotte508@gmail.com>
2018-08-03 22:46:37 +0200

    Update default db

--- app/config/env.ts
+++ app/config/env.ts
@@ -7,5 +7,5 @@ export default {
   minPasswordLength: process.env.minPasswordLength || 6,
   sessionSecret: process.env.sessionSecret || 'Quel est donc le secret mystère du succès de Gaia Project?!',
   port: +process.env.port || 50801,
-  dbUrl: process.env.dbUrl || 'mongodb://gaiaAdmin:This-is-GP-RZ_COYO_508_password!@gaia-project.00h30.com:27019/admin'
+  dbUrl: process.env.dbUrl || 'mongodb://gaiaAdmin:This-is-GP-RZ_COYO_508_password!@www.gaiaform.io:27017/admin'
 };
