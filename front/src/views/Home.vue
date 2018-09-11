cdee1f73bd8a7204362c71b858d44a3d77eeb649
coyotte508 <coyotte508@gmail.com>
2018-09-11 16:57:55 +0200

    Add invite-only message on home page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-body">
         <h2>An online implementation of the Gaia Project board game</h2>
-        <p>Official website <a href="https://www.zmangames.com/en/products/gaia-project/" target="_blank">here</a> (<a href="https://boardgamegeek.com/boardgame/220308/gaia-project" target="_blank">BGG</a><!--, <a href="https://images-cdn.zmangames.com/us-east-1/filer_public/ce/89/ce890bfd-227e-4249-a52a-976bc5f20d19/en_gaia_rulebook_lo.pdf" target="_blank">rules</a>-->)</p>
+        <p>Currently invite-only. Official website <a href="https://www.zmangames.com/en/products/gaia-project/" target="_blank">here</a> (<a href="https://boardgamegeek.com/boardgame/220308/gaia-project" target="_blank">BGG</a><!--, <a href="https://images-cdn.zmangames.com/us-east-1/filer_public/ce/89/ce890bfd-227e-4249-a52a-976bc5f20d19/en_gaia_rulebook_lo.pdf" target="_blank">rules</a>-->)</p>
       </div>
     </div>  
     <br/>
