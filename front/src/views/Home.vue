5d9b49115c2386dfa0b0ada7bf2186dde7ebe76c
coyotte508 <coyotte508@gmail.com>
2018-10-15 19:56:40 +0200

    Change home message

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home text-center container">
     <div class="starter-template">
       <h1>An online implementation of the Gaia Project board game</h1>
-      <p class="lead">Currently invite-only. Official website <a href="https://www.zmangames.com/en/products/gaia-project/" target="_blank">here</a> (<a href="https://boardgamegeek.com/boardgame/220308/gaia-project" target="_blank">BGG</a><!--, <a href="https://images-cdn.zmangames.com/us-east-1/filer_public/ce/89/ce890bfd-227e-4249-a52a-976bc5f20d19/en_gaia_rulebook_lo.pdf" target="_blank">rules</a>-->)</p>
+      <p class="lead">🆕 Playtest rules for the <router-link to="/rules/spaceship">spaceship expansion</router-link> added! <!--Currently invite-only. Official website <a href="https://www.zmangames.com/en/products/gaia-project/" target="_blank">here</a> (<a href="https://boardgamegeek.com/boardgame/220308/gaia-project" target="_blank">BGG</a>--><!--, <a href="https://images-cdn.zmangames.com/us-east-1/filer_public/ce/89/ce890bfd-227e-4249-a52a-976bc5f20d19/en_gaia_rulebook_lo.pdf" target="_blank">rules</a>)--></p>
     </div>  
     <br/>
     <div class="row mb-2">
